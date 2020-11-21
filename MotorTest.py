225f9e457b30ce055ac21161fa37fe7375875e9b
Reed Horton <44300816+ReedsHorton@users.noreply.github.com>
2020-11-20 18:31:23 -0800

    adding rotary_encoder to MotorTest

--- MotorTest.py
+++ MotorTest.py
@@ -1,15 +1,37 @@
 from adafruit_motorkit import MotorKit
 import time
-#import board
+
+import pigpio
+import rotary_encoder
 
 
-kit = MotorKit()
 
-kit.motor1.throttle = 1.0
+pos = 0
+
+def callback(way):
+
+	global pos
+
+	pos += way
+
+	print("pos={}".format(pos))
+
+pi = pigpio.pi()
+
+decoder = rotary_encoder.decoder(pi, 22, 23, callback)
+
+#kit = MotorKit()
+
+#kit.motor1.throttle = 1.0
 #time.sleep(1)
-kit.motor1.throttle = None
+#kit.motor1.throttle = None
 #time.sleep(2)
 #kit.motor1.throttle = -1
 #time.sleep(1)
 #kit.motor1.throttle = 0
 
+time.sleep(300)
+
+decoder.cancel()
+
+pi.stop()
