7a0854a77a4687babb74b8bd0d1133d932783281
Reed Horton <44300816+ReedsHorton@users.noreply.github.com>
2020-11-20 18:49:02 -0800

    checking motor speeds

--- MotorTest.py
+++ MotorTest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from adafruit_motorkit import MotorKit
 import time
+import math
 
 import pigpio
 import rotary_encoder
@@ -10,7 +11,6 @@ import rotary_encoder
 pos = 0
 
 def callback(way):
-
 	global pos
 
 	pos += way
@@ -21,17 +21,65 @@ pi = pigpio.pi()
 
 decoder = rotary_encoder.decoder(pi, 22, 23, callback)
 
-#kit = MotorKit()
+kit = MotorKit()
 
-#kit.motor1.throttle = 1.0
-#time.sleep(1)
+time_now = time.time()
+kit.motor1.throttle = 1.0
+time.sleep(.5)
+
+time_before = time.time()
+pos_before = pos
+time.sleep(5)
+time_now = time.time()
+pos_now = pos
+
+speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+speed = 1/speed * 100
+print("100m = " + str(speed))
+
+
+
+
+time_now = time.time()
+kit.motor1.throttle = -1.0
+time.sleep(.5)
+
+time_before = time.time()
+pos_before = pos
+time.sleep(5)
+time_now = time.time()
+pos_now = pos
+
+speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+speed = 1/speed * 100
+print("100m = " + str(speed))
+
+
+
+time_now = time.time()
+kit.motor1.throttle = .99
+time.sleep(.5)
+
+time_before = time.time()
+pos_before = pos
+time.sleep(5)
+time_now = time.time()
+pos_now = pos
+
+speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+speed = 1/speed * 100
+print("100m = " + str(speed))
+
 #kit.motor1.throttle = None
 #time.sleep(2)
 #kit.motor1.throttle = -1
 #time.sleep(1)
 #kit.motor1.throttle = 0
 
-time.sleep(300)
+time.sleep(1)
 
 decoder.cancel()
 
