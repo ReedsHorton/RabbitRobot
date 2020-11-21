e89eff7d68c34f3adc98a1221890572fafa10b2d
Reed Horton <44300816+ReedsHorton@users.noreply.github.com>
2020-11-20 18:57:03 -0800

    3 seperate speeds, twice each

--- MotorTest.py
+++ MotorTest.py
@@ -25,7 +25,7 @@ kit = MotorKit()
 
 time_now = time.time()
 kit.motor1.throttle = 1.0
-time.sleep(.5)
+time.sleep(1)
 
 time_before = time.time()
 pos_before = pos
@@ -60,6 +60,54 @@ print("100m = " + str(speed))
 
 time_now = time.time()
 kit.motor1.throttle = .99
+time.sleep(.5)
+
+time_before = time.time()
+pos_before = pos
+time.sleep(5)
+time_now = time.time()
+pos_now = pos
+
+speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+speed = 1/speed * 100
+print("100m = " + str(speed))
+
+
+time_now = time.time()
+kit.motor1.throttle = .99
+time.sleep(.5)
+
+time_before = time.time()
+pos_before = pos
+time.sleep(5)
+time_now = time.time()
+pos_now = pos
+
+speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+speed = 1/speed * 100
+print("100m = " + str(speed))
+
+
+time_now = time.time()
+kit.motor1.throttle = .98
+time.sleep(.5)
+
+time_before = time.time()
+pos_before = pos
+time.sleep(5)
+time_now = time.time()
+pos_now = pos
+
+speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+speed = 1/speed * 100
+print("100m = " + str(speed))
+
+
+time_now = time.time()
+kit.motor1.throttle = .98
 time.sleep(.5)
 
 time_before = time.time()
