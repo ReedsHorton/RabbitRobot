5ebeac43f4fcc2104d1d4639ac3672fb29f88a7c
Reed Horton <reedshorton@gmail.com>
2020-11-22 22:34:21 -0500

    DriveTrain class functional

--- motors.py
+++ motors.py
@@ -8,47 +8,68 @@ import math
 import pigpio
 import rotary_encoder
 
-def callback_R(way):
-	global pos_R
+#def callback_R(way):
+#	global pos_R
 
-	pos_R += way
+#	pos_R += way
 
-	print("pos={}".format(pos))
+#	print("pos_R={}".format(pos_R))
 
-def callback_L(way):
-	global pos_L
+#def callback_L(way):
+#	global pos_L
 
-	pos_L += way
+#	pos_L += way
 
-	print("pos={}".format(pos))
+#	print("pos_L={}".format(pos_L))
 
 
 class DriveTrain:
 
+	def callback_R(self, way):
+		self.pos_R += way
+		#print("pos_R={}".format(self.pos_R))
+
+
+	def callback_L(self, way):
+		self.pos_L += way
+		#print("pos_L={}".format(self.pos_L))
+
 	def __init__(self, encoderR_A, encoderR_B, encoderL_A, encoderL_B):
+		print("Initializing...")
 		self.pi = pigpio.pi()
-		self.encoder_R = rotary_encoder.decoder(self.pi, encoderR_A, encoderR_B, callback_R)
-		self.encoder_L = rotary_encoder.decoder(self.pi, encoderL_A, encoderL_B, callback_L)
+		self.encoder_R = rotary_encoder.decoder(self.pi, encoderR_A, encoderR_B, self.callback_R)
+		self.encoder_L = rotary_encoder.decoder(self.pi, encoderL_A, encoderL_B, self.callback_L)
+		self.pos_L = 0
+		self.pos_R = 0
 
 		kit = MotorKit()
 		self.motor_L = kit.motor1
 		self.motor_R = kit.motor2
 
-	def shutdown(self):
+	def drive(self, throttle, dif):
+		self.motor_R.throttle = throttle + dif
+		self.motor_L.throttle = throttle - dif
 
+	def shutdown(self):
+		print("Shutting Down...")
 		self.motor_L.throttle = None
 		self.motor_R.throttle = None
 		self.encoder_R.cancel()
-		self.encoder_L.cancel(())
+		self.encoder_L.cancel()
 		self.pi.stop()
 
 
 if __name__ == '__main__':
 
-	Rabbit = DriveTrain(22, 23, 5, 6)
-	
-	Rabbit.motor_L.throttle = 1
-	time.sleep(1)
-	Rabbit.motor_L.throttle = None
+	R_A = 5
+	R_B = 6
+	L_A = 22
+	L_B = 23
+
+	Rabbit = DriveTrain(R_A, R_B, L_A, L_B)
+
+	Rabbit.drive(.70, .25)	
+	time.sleep(2)
 
 	Rabbit.shutdown()
+
