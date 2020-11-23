4f7abfcef3560736d9eb4e0fd3ee1bb4802df57d
Reed Horton <44300816+ReedsHorton@users.noreply.github.com>
2020-11-22 18:43:31 -0800

    indent

--- MotorTest.py
+++ MotorTest.py
@@ -16,126 +16,126 @@ def callback(way):
 
 
 def main():
-pos = 0
+	pos = 0
 
 
-pi = pigpio.pi()
+	pi = pigpio.pi()
 
-decoder = rotary_encoder.decoder(pi, 22, 23, callback)
+	decoder = rotary_encoder.decoder(pi, 22, 23, callback)
 
-kit = MotorKit()
+	kit = MotorKit()
 
-time_now = time.time()
-kit.motor1.throttle = 1.0
-time.sleep(1)
+	time_now = time.time()
+	kit.motor1.throttle = 1.0
+	time.sleep(1)
 
-time_before = time.time()
-pos_before = pos
-time.sleep(5)
-time_now = time.time()
-pos_now = pos
+	time_before = time.time()
+	pos_before = pos
+	time.sleep(5)
+	time_now = time.time()
+	pos_now = pos
 
-speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+	speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
 
-speed = 1/speed * 100
-print("100m = " + str(speed))
-
-
-
-
-time_now = time.time()
-kit.motor1.throttle = 1.0
-time.sleep(.5)
-
-time_before = time.time()
-pos_before = pos
-time.sleep(5)
-time_now = time.time()
-pos_now = pos
-
-speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
-
-speed = 1/speed * 100
-print("100m = " + str(speed))
+	speed = 1/speed * 100
+	print("100m = " + str(speed))
 
 
 
-time_now = time.time()
-kit.motor1.throttle = .99
-time.sleep(.5)
 
-time_before = time.time()
-pos_before = pos
-time.sleep(5)
-time_now = time.time()
-pos_now = pos
+	time_now = time.time()
+	kit.motor1.throttle = 1.0
+	time.sleep(.5)
 
-speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+	time_before = time.time()
+	pos_before = pos
+	time.sleep(5)
+	time_now = time.time()
+	pos_now = pos
 
-speed = 1/speed * 100
-print("100m = " + str(speed))
+	speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
 
-
-time_now = time.time()
-kit.motor1.throttle = .99
-time.sleep(.5)
-
-time_before = time.time()
-pos_before = pos
-time.sleep(5)
-time_now = time.time()
-pos_now = pos
-
-speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
-
-speed = 1/speed * 100
-print("100m = " + str(speed))
+	speed = 1/speed * 100
+	print("100m = " + str(speed))
 
 
-time_now = time.time()
-kit.motor1.throttle = .98
-time.sleep(.5)
 
-time_before = time.time()
-pos_before = pos
-time.sleep(5)
-time_now = time.time()
-pos_now = pos
+	time_now = time.time()
+	kit.motor1.throttle = .99
+	time.sleep(.5)
 
-speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+	time_before = time.time()
+	pos_before = pos
+	time.sleep(5)
+	time_now = time.time()
+	pos_now = pos
 
-speed = 1/speed * 100
-print("100m = " + str(speed))
+	speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+	speed = 1/speed * 100
+	print("100m = " + str(speed))
 
 
-time_now = time.time()
-kit.motor1.throttle = .98
-time.sleep(.5)
+	time_now = time.time()
+	kit.motor1.throttle = .99
+	time.sleep(.5)
 
-time_before = time.time()
-pos_before = pos
-time.sleep(5)
-time_now = time.time()
-pos_now = pos
+	time_before = time.time()
+	pos_before = pos
+	time.sleep(5)
+	time_now = time.time()
+	pos_now = pos
 
-speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+	speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
 
-speed = 1/speed * 100
-print("100m = " + str(speed))
+	speed = 1/speed * 100
+	print("100m = " + str(speed))
 
-#kit.motor1.throttle = None
-#time.sleep(2)
-#kit.motor1.throttle = -1
-#time.sleep(1)
-#kit.motor1.throttle = 0
 
-time.sleep(1)
+	time_now = time.time()
+	kit.motor1.throttle = .98
+	time.sleep(.5)
 
-kit.motor1.throttle = None
+	time_before = time.time()
+	pos_before = pos
+	time.sleep(5)
+	time_now = time.time()
+	pos_now = pos
 
-decoder.cancel()
+	speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
 
-pi.stop()
+	speed = 1/speed * 100
+	print("100m = " + str(speed))
+
+
+	time_now = time.time()
+	kit.motor1.throttle = .98
+	time.sleep(.5)
+
+	time_before = time.time()
+	pos_before = pos
+	time.sleep(5)
+	time_now = time.time()
+	pos_now = pos
+
+	speed = ((pos_before - pos_now)/48)/(time_now - time_before)*math.pi*.0635
+
+	speed = 1/speed * 100
+	print("100m = " + str(speed))
+
+	#kit.motor1.throttle = None
+	#time.sleep(2)
+	#kit.motor1.throttle = -1
+	#time.sleep(1)
+	#kit.motor1.throttle = 0
+
+	time.sleep(1)
+
+	kit.motor1.throttle = None
+
+	decoder.cancel()
+
+	pi.stop()
 
 
 
